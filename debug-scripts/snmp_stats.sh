e2faf0f870ba179f9991acd48e2ea3a4d60c6b64
Samuel DeLaughter <samuel.de-laughter@inria.fr>
2026-04-28 10:50:59 +0200

    snmp_stats script

---
+++ debug-scripts/snmp_stats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+iters=$1
+logfile=$2
+interval="${3:-1}"
+
+for ((i=0; i<$1; i++)); do
+    echo "$i: $(date --rfc-3339='ns' -u)" >> $logfile
+    cat /proc/net/snmp >> $logfile
+    sleep $interval
+done
